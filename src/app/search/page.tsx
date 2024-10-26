1a6bedc225b43e5bf70ed292a6b94d7dc477cc9c
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2024-10-26 22:18:10 +0600

    some datra

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,35 +1,6 @@
-'use client';
+import Searchbar from "@/components/Searchbar";
 
-import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
-import MovieList from '@/components/MovieList';
 
-function Search() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query");
-
-  return (
-    <div>
-      {query ? (
-        <>
-          <h1 className="text-2xl font-semibold py-4">
-            Search Results for: {query}
-          </h1>
-          <MovieList query={query} />
-        </>
-      ) : (
-        <p className="text-lg">Please enter a search query.</p>
-      )}
-    </div>
-  );
+export default function Page() {
+  return <Searchbar />;
 }
-
-export function Searchbar() {
-  return (
-    <Suspense fallback={<p>Loading search results...</p>}>
-      <Search />
-    </Suspense>
-  );
-}
-
-export default Searchbar;
---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,35 @@
+// src/components/Searchbar.js or .tsx
+
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import MovieList from '@/components/MovieList';
+
+function Search() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  return (
+    <div>
+      {query ? (
+        <>
+          <h1 className="text-2xl font-semibold py-4">
+            Search Results for: {query}
+          </h1>
+          <MovieList query={query} />
+        </>
+      ) : (
+        <p className="text-lg">Please enter a search query.</p>
+      )}
+    </div>
+  );
+}
+
+export default function Searchbar() {
+  return (
+    <Suspense fallback={<p>Loading search results...</p>}>
+      <Search />
+    </Suspense>
+  );
+}
