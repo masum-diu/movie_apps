c625f919a9d9df6b2a95d0ed159c4d828cfcf72e
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2024-10-26 22:12:25 +0600

    some datra

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,34 +1,35 @@
-// /app/search/page.js
+'use client';
 
-"use client";
-export const dynamic = "force-dynamic";
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import MovieList from '@/components/MovieList';
 
-import { useSearchParams } from "next/navigation";
-import MovieList from "@/components/MovieList";
-import { Suspense } from "react";
-
-const Search = () => {
+function Search() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
 
   return (
+    <div>
+      {query ? (
+        <>
+          <h1 className="text-2xl font-semibold py-4">
+            Search Results for: {query}
+          </h1>
+          <MovieList query={query} />
+        </>
+      ) : (
+        <p className="text-lg">Please enter a search query.</p>
+      )}
+    </div>
+  );
+}
+
+export function Searchbar() {
+  return (
     <Suspense fallback={<p>Loading search results...</p>}>
-      <div>
-        {query ? (
-          <>
-            <h1 className="text-2xl font-semibold py-4">
-              Search Results for: {query}
-            </h1>
-            <Suspense fallback={<p>Loading movies...</p>}>
-              <MovieList query={query} />
-            </Suspense>
-          </>
-        ) : (
-          <p className="text-lg">Please enter a search query.</p>
-        )}
-      </div>
+      <Search />
     </Suspense>
   );
-};
+}
 
-export default Search;
+export default Searchbar;
